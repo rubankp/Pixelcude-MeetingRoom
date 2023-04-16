f9c9b6b562ae475bd81ff0bebed1a8e3ec882187
Manikanda Ruban <kpmanikandaruban@gmail.com>
2023-04-16 22:58:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// Dependencies
+import React from "react";
+
+// Styles
+import "./tailwind.output.css";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-500">
+      <h2 className="text-center text-4xl font-bold text-blue-900">TailwindCSS + React</h2>
+    </div>
+  );
+};
+
+export default App;
